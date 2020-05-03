4254ecdb9ef0044a9c79d753560c01cfa5e85407
acthacker <53494387+acthacker@users.noreply.github.com>
2020-05-03 15:58:23 +0200

    Updating to prep for second blog post

---
+++ birthday_explore.py
@@ -0,0 +1,7 @@
+from birthday_problem import exponentiation
+from birthday_problem import factorial_equation
+import pandas as pd
+
+congress_data = pd.read_csv("congress_data.csv")
+
+print((congress_data).head())
